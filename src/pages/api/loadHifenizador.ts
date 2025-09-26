2e362a2a6c8187a7b69d7c6babb9ee9ec08551f9
renribsilva <ren.rib.silva@gmail.com>
2025-09-26 15:08:03 -0300

    arquivos modificados

--- src/pages/api/loadHifenizador.ts
+++ src/pages/api/loadHifenizador.ts
@@ -3,11 +3,21 @@ import { hifenizador } from "../../lib/hifenizador";
 import fs from 'fs';
 import path from 'path';
 
-const hyph_path = path.join(process.cwd(), 'public', 'hyph_pt_BR.dic');
-const rawPatterns = fs.readFileSync(hyph_path, 'utf-8')
+const hyph_path = path.join(process.cwd(), 'public', 'hyph_pt_BR.dic')
+const my_hyph_path = path.join(process.cwd(), 'public', 'my_hyph.dic')
+
+const myRules = fs.readFileSync(my_hyph_path, 'utf-8')
+  .split(/\r?\n/)
+  .map(line => line.trim())
+const libreRules = fs.readFileSync(hyph_path, 'utf-8')
   .split(/\r?\n/)
   .map(line => line.trim())
   .filter(line => line.length > 0 && !line.match("UTF-8") && !line.match("4'4"));
+
+const allPatterns = Array.from(new Set([
+  ...myRules,
+  ...libreRules
+]));
 
 // Handler da API
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
@@ -17,7 +27,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return res.status(400).json({ error: "Parâmetro 'word' é obrigatório" });
   }
 
-  const result = hifenizador(word, rawPatterns);
+  const result = hifenizador(word, allPatterns);
 
   return res.status(200).json(result);
 }
